3187f8f71063be5948891d42f9d6b23b594c4e5a
Efraim <45230414+EfraimGENC@users.noreply.github.com>
2022-09-06 02:06:41 +0300

    class added to init

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name="ups_turkey",
-    version="1.0.0-beta.2",
+    version="1.0.0-beta.3",
     author="Efraim GENC",
     author_email='info@kavimdigital.com',
     description="Easy integration for UPS Turkey",
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/EfraimGENC/ups-turkey",
-    download_url = 'https://github.com/EfraimGENC/ups-turkey/archive/refs/tags/v1.0.0.tar.gz',
+    download_url = 'https://github.com/EfraimGENC/ups-turkey/archive/refs/tags/v1.0.0-beta.3.tar.gz',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
